10288392bde7b0aff8677990f70edbe0c290239d
Andrew Stuart <andrew.stuart2@gmail.com>
2023-03-03 20:27:55 -0700

    Add file upload, download, get, list, and delete.

--- files.go
+++ files.go
@@ -34,7 +34,7 @@ func (t *Time) UnmarshalJSON(bs []byte) error {
 	return nil
 }
 
-type FileUploadRes struct {
+type FileRes struct {
 	ID        string `json:"id"`
 	Object    string `json:"object"`
 	Bytes     int    `json:"bytes"`
@@ -45,7 +45,7 @@ type FileUploadRes struct {
 
 // Upload uploads a file to be used within the OpenAI plaform. Most often for
 // fine-tuning models.
-func (c Client) Upload(ctx context.Context, v FileUploadReq) (*FileUploadRes, error) {
+func (c Client) Upload(ctx context.Context, v FileUploadReq) (*FileRes, error) {
 	body := &bytes.Buffer{}
 	w := multipart.NewWriter(body)
 	image, err := w.CreateFormFile("file", v.Filename)
@@ -62,7 +62,7 @@ func (c Client) Upload(ctx context.Context, v FileUploadReq) (*FileUploadRes, er
 
 	w.Close()
 
-	var res FileUploadRes
+	var res FileRes
 	err = c.c.R().
 		Post("files").
 		SetHeader("Content-Type", "multipart/form-data; boundary="+w.Boundary()).
@@ -73,4 +73,46 @@ func (c Client) Upload(ctx context.Context, v FileUploadReq) (*FileUploadRes, er
 		return nil, err
 	}
 	return &res, nil
+}
+
+// FileListRes represents the list of files managed by OpenAI
+type FileListRes struct {
+	Data   []FileRes `json:"data"`
+	Object string    `json:"object"`
+}
+
+// ListFiles returns the list of files known to OpenAI
+func (c Client) ListFiles(ctx context.Context) (*FileListRes, error) {
+	var res FileListRes
+	err := c.c.R().
+		Get("files").
+		Do(ctx).
+		JSON(&res)
+	if err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+// GetFileDetails returns the File details for an individual file managed by
+// the OpenAI APIs.
+func (c Client) GetFileDetails(ctx context.Context, id string) (*FileRes, error) {
+	var res FileRes
+	err := c.c.R().Get("/files/%s", id).Do(ctx).JSON(&res)
+	if err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+// DeleteFile deletes the file specified.
+func (c Client) DeleteFile(ctx context.Context, id string) error {
+	return c.c.R().Delete("files/%s", id).Do(ctx).Err()
+}
+
+// DownloadFile returns a file as an io.ReadCloser. For now, any API errors will be
+// returned in the io.Read method, though this will likely change, hence the
+// error return.
+func (c Client) DownloadFile(ctx context.Context, id string) (io.ReadCloser, error) {
+	return c.c.R().Get("/files/%s/content", id).Do(ctx), nil
 }
